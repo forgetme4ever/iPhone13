36fbbb300a15699250892d16d14dff1d01263e57
Eugen Burkenia <burikievgen@gmail.com>
2022-01-24 18:38:08 -0500

    clear form, close the form after submitting

--- scripts/sendForm.js
+++ scripts/sendForm.js
@@ -1,47 +1,51 @@
-'use strict';
+"use strict";
 
 const sendForm = () => {
-    const btnOpenModal = document.querySelector('.card-details__button_delivery');
-    const cardDetailsTitle = document.querySelector('.card-details__title');
-    const modal = document.querySelector('.modal');
-    const modalTitle = modal.querySelector('.modal__title');
-    const modalClose = modal.querySelector('.modal__close');
-    const modalForm = modal.querySelector('form');
+  const btnOpenModal = document.querySelector(".card-details__button_delivery");
+  const cardDetailsTitle = document.querySelector(".card-details__title");
+  const modal = document.querySelector(".modal");
+  const modalTitle = modal.querySelector(".modal__title");
+  const modalClose = modal.querySelector(".modal__close");
+  const modalForm = modal.querySelector("form");
 
-    btnOpenModal.addEventListener('click', () => {
-        modal.style.display = 'flex';
-        modalTitle.textContent = cardDetailsTitle.textContent;
+  btnOpenModal.addEventListener("click", () => {
+    modal.style.display = "flex";
+    modalTitle.textContent = cardDetailsTitle.textContent;
+    const labels = modal.querySelectorAll(".modal__label");
+
+    labels.forEach((label) => {
+      const input = label.querySelector("input");
+      input.value = "";
     });
+  });
 
-    modalClose.addEventListener('click', () => {
-        modal.style.display = 'none';
-    })
+  modalClose.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
 
-    modalForm.addEventListener('submit' , (event) => {
-        event.preventDefault();
+  modalForm.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-        const labels = modal.querySelectorAll('.modal__label');
-        const sendMessage = {};
+    const labels = modal.querySelectorAll(".modal__label");
+    const sendMessage = {};
 
-        labels.forEach(label => {
-            const span = label.querySelector('span');
-            const input = label.querySelector('input');
+    labels.forEach((label) => {
+      const span = label.querySelector("span");
+      const input = label.querySelector("input");
 
-            sendMessage[span.textContent] = input.value;
-        })
-        fetch('https://jsonplaceholder.typicode.com/posts', {
-            method: 'POST',
-            body: JSON.stringify(sendMessage),
-            headers: {
-              'Content-type': 'application/json; charset=UTF-8',
-            },
-          })
-            .then(() => {
-                console.log('Data has been sent');
-            })
-            
-          
-    })
-}
+      sendMessage[span.textContent] = input.value;
+    });
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify(sendMessage),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }).then(() => {
+      console.log("Data has been sent");
+      modal.style.display = "none";
+    });
+  });
+};
 
 sendForm();
